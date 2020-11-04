ca451a9d0f37d20714463539186427c77c3bf620
MaeglynD <maeglyn.davies@gmail.com>
2020-11-04 00:11:58 +0000

    created boilerplate, setup configs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import {
+	CarouselProvider, Slider, Slide,
+} from 'pure-react-carousel';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="l-app">
+				{/* Image & image selection container */}
+				<div className="l-portion-a">
+					<CarouselProvider
+						naturalSlideWidth={100}
+						naturalSlideHeight={125}
+						totalSlides={3}
+					>
+						<Slider>
+							<Slide index={0}>I am the first Slide.</Slide>
+							<Slide index={1}>I am the second Slide.</Slide>
+							<Slide index={2}>I am the third Slide.</Slide>
+						</Slider>
+					</CarouselProvider>
+					<div className="l-slides">
+						{/*  */}
+					</div>
+				</div>
+				{/* Info container */}
+				<div className="l-portion-b">
+					{/*  */}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
